670736ee2e18e4c58f91588562d2737b45c969f4
felipemenke <felipelm4@al.insper.edu.br>
2024-04-24 11:23:17 -0300

    criação do mapa

---
+++ criandotamanho.py
@@ -0,0 +1,15 @@
+def cria_mapa (n):
+    lista=[]
+    sublista=[]
+
+    i=0
+    while i<n:
+        sublista.append(' ')
+        i+=1
+    j=0
+    while j<n:
+        lista.append(sublista)
+        j+=1
+        
+    return lista
+print(cria_mapa(4))
