509dfe5039d7311bcee60acb5c7389e4c6860a81
TiagoRibe <tiagocr1@al.insper.edu.br>
2024-04-25 14:29:41 -0300

    exercicio2

--- 2navioalocado.py
+++ 2navioalocado.py
@@ -1,9 +1,17 @@
 def posicao_suporta (mapa,blocos,linha,coluna,ori): 
     
-    
     if ori=='v':
         if linha+blocos > len(mapa):
+            return False  
+    if ori=='h':
+        if coluna+blocos > len(mapa):
             return False
+    for i in range(blocos):
+        if ori == 'v':
+            if mapa[linha + i][coluna] != ' ':
+                return False
+        else:  
+            if mapa[linha][coluna + i] != ' ':
+                return False
 
-
-            
+    return True
