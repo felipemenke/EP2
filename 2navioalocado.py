d0decd57b1818da8afb2f049cf38834c1515fee0
TiagoRibe <tiagocr1@al.insper.edu.br>
2024-04-25 11:26:13 -0300

    correcaoAlocado

--- 2navioalocado.py
+++ 2navioalocado.py
@@ -1,27 +1,9 @@
 def posicao_suporta (mapa,blocos,linha,coluna,ori): 
     
+    
     if ori=='v':
-        for j in mapa[linha][coluna]:
-            k=0
-            count=0
-            while k < blocos:
-                if j==' ':
-                    count+=1
-                    
-                k+=1
-            if count<blocos:
-                return False
-            else:
-                return True
-    else:
-        for i in mapa[linha][coluna]:
-            k=0
-            count=0
-            while k < blocos:
-                if i==' ':
-                    count+=1
-                k+=1
-            if count<blocos:
-                return False
-            else:
-                return True
+        if linha+blocos > len(mapa):
+            return False
+
+
+            
